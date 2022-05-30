3032e96f14c017ac6eb6b4b4088ca58bd2a847fd
Derrint <lorensioderrint@gmail.com>
2022-05-30 16:45:11 +0700

    remove unused components

--- src/components/page/home/index.ts
+++ src/components/page/home/index.ts
@@ -1,21 +1,6 @@
-import AdNetwork from './ad-network';
 import Banner from './banner';
 import ContactUs from './contact-us';
 import Features from './features';
 import Highlights from './highlights';
-import Offers from './offers';
-import Portfolio from './portfolio';
-import Services from './services';
-import Spotlight from './spotlight';
 
-export {
-  Banner,
-  Features,
-  Offers,
-  Services,
-  Spotlight,
-  AdNetwork,
-  ContactUs,
-  Portfolio,
-  Highlights,
-};
+export { Banner, Features, ContactUs, Highlights };
