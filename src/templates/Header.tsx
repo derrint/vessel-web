762d88240fd82cef5bd62e85dd8a4b6ee0b15927
Derrint <lorensioderrint@gmail.com>
2022-06-23 13:54:13 +0700

    Header : disable fade in animation due to glitch on first load

--- src/templates/Header.tsx
+++ src/templates/Header.tsx
@@ -6,7 +6,6 @@ import { LinkOut } from 'akar-icons';
 import Link from 'next/link';
 import Router, { useRouter } from 'next/router';
 import { FaDiscord, FaTwitter } from 'react-icons/fa';
-import { Fade } from 'react-reveal';
 
 import { Background } from '@components/background';
 import { DropdownMenu } from '@components/dropdown';
@@ -94,119 +93,117 @@ const Header = () => {
         state.isAnimationDone ? '' : ''
       }`}
     >
-      <Fade top duration={750} delay={0} when={state.isReady}>
-        <Section yPadding="py-5 md:py-6" className="relative" isHeader>
-          <NavbarTwoColumns
-            logo={
-              <img
-                src="/assets/images/logos/logo.png"
-                alt=""
-                className="h-6 md:h-8 lg:h-9 aspect-auto"
-              />
-            }
-          >
-            {menus.map(
-              ({
-                id,
-                label,
-                href,
-                submenus,
-                isButton,
-                isDesktop,
-                isMobile,
-                isExternalLink,
-                goToSectionIndex,
-              }: any) => (
-                <li
-                  key={id}
-                  className={`${
-                    isDesktop && !isMobile ? 'hidden md:block' : ''
-                  } ${!isDesktop && isMobile ? 'md:hidden' : ''}`}
-                >
-                  <div className="text-right">
-                    {submenus ? (
-                      <DropdownMenu
-                        title={label}
-                        items={submenus}
-                        onChange={(v: any) => {
-                          const currentSubmenu = submenus.find(
-                            (x: any) => x.id === v
-                          );
-                          if (
-                            router.pathname === '/' &&
+      <Section yPadding="py-5 md:py-6" className="relative" isHeader>
+        <NavbarTwoColumns
+          logo={
+            <img
+              src="/assets/images/logos/logo.png"
+              alt=""
+              className="h-6 md:h-8 lg:h-9 aspect-auto"
+            />
+          }
+        >
+          {menus.map(
+            ({
+              id,
+              label,
+              href,
+              submenus,
+              isButton,
+              isDesktop,
+              isMobile,
+              isExternalLink,
+              goToSectionIndex,
+            }: any) => (
+              <li
+                key={id}
+                className={`${
+                  isDesktop && !isMobile ? 'hidden md:block' : ''
+                } ${!isDesktop && isMobile ? 'md:hidden' : ''}`}
+              >
+                <div className="text-right">
+                  {submenus ? (
+                    <DropdownMenu
+                      title={label}
+                      items={submenus}
+                      onChange={(v: any) => {
+                        const currentSubmenu = submenus.find(
+                          (x: any) => x.id === v
+                        );
+                        if (
+                          router.pathname === '/' &&
+                          currentSubmenu.goToSectionIndex
+                        ) {
+                          fullPage.methods.moveTo(
                             currentSubmenu.goToSectionIndex
-                          ) {
-                            fullPage.methods.moveTo(
-                              currentSubmenu.goToSectionIndex
-                            );
-                          } else {
-                            Router.push(currentSubmenu?.href as any);
-                          }
-                        }}
-                        classNames={{
-                          itemsWrapper: '!mt-4 !rounded-xl',
-                          button: 'font-medium',
-                        }}
-                      />
-                    ) : router.pathname === '/' && goToSectionIndex ? (
+                          );
+                        } else {
+                          Router.push(currentSubmenu?.href as any);
+                        }
+                      }}
+                      classNames={{
+                        itemsWrapper: '!mt-4 !rounded-xl',
+                        button: 'font-medium',
+                      }}
+                    />
+                  ) : router.pathname === '/' && goToSectionIndex ? (
+                    <a
+                      className={`text-base font-medium hover:text-primary flex items-center cursor-pointer ${
+                        isButton
+                          ? 'bg-primary px-4 lg:px-6 py-2 lg:py-4 rounded-full'
+                          : ''
+                      }
+                          ${router.pathname === href ? 'text-primary' : ''}
+                          `}
+                      onClick={() => {
+                        fullPage.methods.moveTo(goToSectionIndex);
+                      }}
+                    >
+                      {label}
+                    </a>
+                  ) : (
+                    <Link href={href} passHref>
                       <a
-                        className={`text-base font-medium hover:text-primary flex items-center cursor-pointer ${
+                        className={`text-base font-medium hover:text-primary flex items-center ${
                           isButton
                             ? 'bg-primary px-4 lg:px-6 py-2 lg:py-4 rounded-full'
                             : ''
                         }
                           ${router.pathname === href ? 'text-primary' : ''}
                           `}
-                        onClick={() => {
-                          fullPage.methods.moveTo(goToSectionIndex);
-                        }}
+                        target={isExternalLink ? '_blank' : ''}
                       >
                         {label}
+                        {isExternalLink && (
+                          <div className="ml-2">
+                            <LinkOut strokeWidth={3} size={16} />
+                          </div>
+                        )}
                       </a>
-                    ) : (
-                      <Link href={href} passHref>
-                        <a
-                          className={`text-base font-medium hover:text-primary flex items-center ${
-                            isButton
-                              ? 'bg-primary px-4 lg:px-6 py-2 lg:py-4 rounded-full'
-                              : ''
-                          }
-                          ${router.pathname === href ? 'text-primary' : ''}
-                          `}
-                          target={isExternalLink ? '_blank' : ''}
-                        >
-                          {label}
-                          {isExternalLink && (
-                            <div className="ml-2">
-                              <LinkOut strokeWidth={3} size={16} />
-                            </div>
-                          )}
-                        </a>
-                      </Link>
-                    )}
-                  </div>
-                </li>
-              )
-            )}
-            <div className="pt-1 flex items-center gap-3">
-              <Link href={'https://discord.gg/DZrNRBkmzt'} passHref>
-                <a target={'_blank'}>
-                  <div className="rounded-full p-1 bg-black text-white">
-                    <FaDiscord size={14} />
-                  </div>
-                </a>
-              </Link>
-              <Link href={'https://twitter.com/vesselnfts'} passHref>
-                <a target={'_blank'}>
-                  <div className="rounded-full p-1 bg-black text-white">
-                    <FaTwitter size={14} />
-                  </div>
-                </a>
-              </Link>
-            </div>
-          </NavbarTwoColumns>
-        </Section>
-      </Fade>
+                    </Link>
+                  )}
+                </div>
+              </li>
+            )
+          )}
+          <div className="pt-1 flex items-center gap-3">
+            <Link href={'https://discord.gg/DZrNRBkmzt'} passHref>
+              <a target={'_blank'}>
+                <div className="rounded-full p-1 bg-black text-white">
+                  <FaDiscord size={14} />
+                </div>
+              </a>
+            </Link>
+            <Link href={'https://twitter.com/vesselnfts'} passHref>
+              <a target={'_blank'}>
+                <div className="rounded-full p-1 bg-black text-white">
+                  <FaTwitter size={14} />
+                </div>
+              </a>
+            </Link>
+          </div>
+        </NavbarTwoColumns>
+      </Section>
     </Background>
   );
 };
